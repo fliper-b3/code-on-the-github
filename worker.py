5b81d4aafd71ae63b4195e0517b17cefd6bff179
fliper-b3 <fliper-b3@yandex.ru>
2020-11-04 10:49:52 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -20,45 +20,64 @@ prmt_meta = PrometheusMeta(
     headers={"Content-Type": "application/x-www-form-urlencoded"}
 )
 
-big_q = [
-    { 'query': f'avg by (namespace) (avg_over_time(kube_deployment_created{{deployment="kube-controlplane",namespace=~"cp-[0-9]+"}}[{day+1}d]))'} 
-    for day in range(3)
-]
+class Worker:
 
-##########################################################################################################################################################
-########################      KАК ВОТ ЭТО ПРЕВРАТИТЬ В КЛАСС назавем его Worker + СМ ВОПРОС В MAIN      ##################################################
-##########################################################################################################################################################
-async def fetch(client, data_post):
-    async with client.post(url=prmt_meta.url, data=data_post) as resp:
-        assert resp.status == 200
-        return await resp.json()
+    _task_queue = []
+    _result_data = []
 
+    def __init__(self, url, login, password, headers):
+        self.url = url
+        self.login = login
+        self.password = password
+        self.headers = headers
+        self.auth = aiohttp.BasicAuth(login=prmt_meta.login,
+                                 password=prmt_meta.passwd)
 
-async def queue(client, queries):
-    tasks = []
-    for q in queries:
-        ob = asyncio.ensure_future(
-            fetch(client, q)
-        )
-        tasks.append(ob)
-    return await asyncio.gather(*tasks)
+    def set_queue(self, q):
+        self._task_queue = q
 
+    def get_queue(self):
+        return self._task_queue
 
-async def main():
-    headers = prmt_meta.headers
-    auth = aiohttp.BasicAuth(login=prmt_meta.login, password=prmt_meta.passwd)
-    async with aiohttp.ClientSession(auth=auth, headers=headers) as session:
-        return await queue(session, big_q)
+    async def fetch(self, client, data_post):
+        async with client.post(url=self.url, data=data_post) as resp:
+            assert resp.status == 200
+            return await resp.json()
 
+    async def queue(self, client):
+        tasks = []
+        for q in self._task_queue:
+            ob = asyncio.ensure_future(
+                self.fetch(client, q)
+            )
+            tasks.append(ob)
+        return await asyncio.gather(*tasks)
+
+    async def main(self):
+        if not self._task_queue:
+            raise ("Queue is empty")
+        async with aiohttp.ClientSession(auth=self.auth, headers=self.headers) as session:
+            return await self.queue(session)
+
+    def get_data(self):
+        return self._result_data
+
+    def run(self):
+        self._result_data = asyncio.run(self.main())
 
 if __name__ == '__main__':
-    # Хочу что бы было как то так
-    # worker = Worker(
-    #    url=prtm.url,
-    #    login=prtm.login,
-    #    pass=prtm.pass
-    # )
-    # worker.big_q=[<BIG QUEUE HERE>]
-    # result = worker.run()
-    
-    print(asyncio.run(main()))
+    w = Worker(
+        url=prmt_meta.url,
+        login=prmt_meta.login,
+        password=prmt_meta.passwd,
+        headers=prmt_meta.headers,
+    )
+    w.set_queue(
+        [
+            {
+                'query': f'avg by (namespace) (avg_over_time(kube_deployment_created{{deployment="kube-controlplane",namespace=~"cp-[0-9]+"}}[{day + 1}d]))'}
+            for day in range(3)
+        ]
+    )
+    w.run()
+    print(w.get_data())
